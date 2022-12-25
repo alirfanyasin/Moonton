6654920a7565b99eeb7dc35150e0a74b2f2bf8ab
alirfanyasin <risayunita21@gmail.com>
2022-12-25 19:11:20 +0700

    feat: slicing signin and signup

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,30 @@ use Inertia\Inertia;
 */
 
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::redirect('/', '/prototype/login');
+
+
+Route::prefix('prototype')->name('prototype.')->group(function () {
+    Route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+    Route::get('/register', function () {
+        return Inertia::render('Prototype/Register');
+    })->name('register');
 });
+
+
+
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
